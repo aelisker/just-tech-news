bdb4fff5f93aceb72f70e55b0f4bc3c20d136e14
Aaron Lisker <aelisker@gmail.com>
2020-05-29 22:21:23 -0700

    added User model

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create user model
+class User extends Model {}
+
+// define table colums and configs
+User.init(
+  {
+    // define id column
+    id: {
+      // use special Sequelize Datatypes object provie what type of data it is
+      type: Datatypes.INTEGER,
+      // thbis is equivalent of sqls not null option
+      allowNull: false,
+      // instruct that this is primary key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define username column
+    username: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    // define email column
+    email: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      // don't allow duplicate emails
+      unique: true,
+      // if allowNull is set to false, we can run our data through validator before creating tables
+      validate: {
+        isEmail: true
+      }
+    },
+    // define password column
+    password: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        // means password must be at least four char long
+        len: [4]
+      }
+    }
+  },
+  {
+    // table config options go here
+    // pass in our imported sequelize connection (the direct connecvtion to our database)
+    sequelize,
+    // don't auto create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of db table
+    freezeTableName: true,
+    // use underscores instead of camel case
+    underscored: true,
+    // make model stay lowercase in db
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
