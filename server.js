2790708831f9f12e8a5d2a0102c44bdd2e9a2ec4
Aaron Lisker <aelisker@gmail.com>
2020-06-06 12:55:44 -0700

    able to login and logout, storing session using cookie

--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: process.env.DB_COOKIE,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
